626f0276b4cb3a4abc7eabe889421b0c2f6772e6
najahto <tahaomarnajah@gmail.com>
2022-01-30 15:09:03 +0100

    feat: exclude login, register, get products & categories routes from auth

--- ecom-backend/middleware/auth-jwt.js
+++ ecom-backend/middleware/auth-jwt.js
@@ -1,11 +1,18 @@
 const expressJwt = require('express-jwt');
 
 function authJWT() {
+  const api = process.env.APP_URL;
   const secret = process.env.JWT_SECRET;
   return expressJwt({
     secret,
-      algorithms: ['HS256'],
-
+    algorithms: ['HS256'],
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      `${api}/users/login`,
+      `${api}/users/register`,
+    ],
   });
 }
 
--- ecom-backend/routes/users.js
+++ ecom-backend/routes/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
 
-router.post('/', userController.registerUser);
+router.post('/register', userController.registerUser);
+
+router.post('/login', userController.userLogin);
 
 router.get('/', userController.getUsers);
 
@@ -10,6 +12,5 @@ router.get('/:id', userController.findUser);
 
 router.put('/:id', userController.updateUser);
 
-router.post('/login', userController.userLogin);
 
 module.exports = router;
