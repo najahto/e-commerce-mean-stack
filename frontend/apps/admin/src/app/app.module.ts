b3280555862abb9307efb20d7f0affb6fc94202f
najahto <tahaomarnajah@gmail.com>
2022-02-14 23:35:56 +0100

    feat(admin): added content & sidebar components

--- frontend/apps/admin/src/app/app.module.ts
+++ frontend/apps/admin/src/app/app.module.ts
@@ -2,14 +2,38 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContentComponent } from './shared/content/content.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+      },
+      {
+        path: 'categories',
+        component: DashboardComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    ContentComponent,
+    SidebarComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
