933ff2e5e82c43a4fdd5f9ab604d8bb962e16831
najahto <tahaomarnajah@gmail.com>
2022-02-22 21:51:22 +0100

    feat(auth): add logout

--- frontend/apps/admin/src/app/shared/sidebar/sidebar.component.ts
+++ frontend/apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@frontend/users';
 
 @Component({
   selector: 'admin-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout() {
+    console.log(
+      '🚀 ~ file: sidebar.component.ts ~ line 15 ~ SidebarComponent ~ logout ~ logout'
+    );
+    this.authService.logout();
   }
-
 }
